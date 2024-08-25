361cb4ea064ef75fdcc24fc1729dbb5151156552
Askeiroh <zanpakutou.akira@gmail.com>
2024-08-25 20:04:19 -0300

    first setup on windows

--- cses/building-roads/test1.cpp
+++ cses/building-roads/test1.cpp
@@ -6,30 +6,18 @@ using namespace std;
 #define int long long
 
 const int MAX = 1e5 + 5;
-int n, m;
+int n, m;                                                                 vvvvvv
 vector<int> adj[MAX];
 vector<bool> vis(MAX);
-int color[MAX];
 bool OK = true;
 
 
-void coloring_dfs(int u) {
+void dfs(int u) {
     vis[u] = true;
     
     for(int v : adj[u]){
         if (vis[v] == 0) {
-            if (color[u] == 1){
-                color[v] = 2;
-            } else {
-                color[v] = 1;
-            }
-            coloring_dfs(v);
-            if(!OK) return;
-        } else {
-            if (color[v] == color[u]) {
-                OK = false;
-                return;
-            }
+            dfs(v);
         }
 }}
 
@@ -39,28 +27,18 @@ void solve() {
     for(int i = 0; i < m; i++) {
         int u, v;
         cin >> u >> v;
-        u--;
-        v--;
+        u--; v--;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
 
     for (int i = 0; i < n; i++) { 
         if (!vis[i]) {
-            color[i] = 1;
-            coloring_dfs(i);
+            dfs(i);
         }
     }
-
-    if (OK) {
-        for (int i = 0; i < n; i++) {
-            cout << color[i] << " ";
-        }
-        cout << endl;
-    } else {
-        cout << "IMPOSSIBLE" << endl;    }
 }
-
+1
 signed main() {
     ios_base::sync_with_stdio(0);cin.tie(0);
     int TC = false;
